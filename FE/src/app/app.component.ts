5f3c3174c03046706893f606a286c89b67275349
Shanre Scheepers <21100387@virtualwindow.co.za>
2023-11-01 05:28:27 +0200

    feat : studio screen done

--- FE/src/app/app.component.ts
+++ FE/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+<div>
+    <nav [class.hide-nav]="isHiddenRoute()" class="nav">
+        <div class="nav-item-logo-div">
+            <img src="../assets/images/gemma-logo-icon.png" alt="Logo" class="nav-item-logo">
+        </div>
+        <div class="nav-grid">
+            
+          
+            <button class="nav-item"><a routerLink="/mappinglibrary" class="nav-link">Gesture Library</a></button>
+            <button class="nav-item"><a routerLink="/gemmastudio" class="nav-link">Gemma Studio</a></button>
+            <button class="nav-item"><a routerLink="/helphub" class="nav-link">Help Hub</a></button>
+        </div>
+    </nav>
+</div>
+  <router-outlet></router-outlet>
+`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gemma';
+  constructor(private router: Router) { }
+
+  isHiddenRoute(): boolean {
+    const hiddenRoutes = ['/', '/onboarding1', '/onboarding2'];
+    return hiddenRoutes.includes(this.router.url);
+  }
 }
